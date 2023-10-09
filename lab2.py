76d7b2d08b3c5acc5866ba4ece3a2c53673043ce
Rtiscev <alexandru.rtiscev@isa.utm.md>
2023-10-09 21:27:57 +0300

    Add exit out of the loop

--- lab2.py
+++ lab2.py
@@ -35,37 +35,45 @@ finalString = response.text[startIndex : endIndex + len("<tbody>")]
 #     file.write(finalString)
 
 currentposition = 0
-while True:
-    startCenter = finalString.find('<tr align="center">', currentposition)
-    endCenter = finalString.find("</tr>", startCenter)
-    currentposition = endCenter + len("</tr>")
+with open ("dataset.csv","w") as dataset:
+    appendData=[]
+    while True:
+        toBeAppended=""
+        
+        startCenter = finalString.find('<tr align="center">', currentposition)
+        if startCenter==-1:
+            break
+        endCenter = finalString.find("</tr>", startCenter)
+        currentposition = endCenter + len("</tr>")
 
-    finalCenter = finalString[startCenter : endCenter + len("</tr>")]
+        finalCenter = finalString[startCenter : endCenter + len("</tr>")]
 
-    arr = []
-    incremental = 0
+        arr = []
+        incremental = 0
 
-    while finalCenter.find("<td", incremental) != -1:
-        beginningIndex = finalCenter.find("<td", incremental)
-        endingIndex = finalCenter.find("</td>", beginningIndex)
-        incremental = endingIndex
-        string = finalCenter[beginningIndex : endingIndex + len("</td>")]
-        arr.append(string)
+        while finalCenter.find("<td", incremental) != -1:
+            beginningIndex = finalCenter.find("<td", incremental)
+            endingIndex = finalCenter.find("</td>", beginningIndex)
+            incremental = endingIndex
+            string = finalCenter[beginningIndex : endingIndex + len("</td>")]
+            arr.append(string)
 
-    # 0 counter
-    print(getSimpleParts(arr[0], "<td class=first>", "</td>"))
-    # 1D temp
-    print(getSimpleParts(arr[1], "<td class='first_in_group positive'>", "</td>"))
-    # 2D pressure
-    print(getSimpleParts(arr[2], "<td>", "</td>"))
-    # 5D wind
-    print(getSimpleParts(arr[5], "<td><span>", "</span></td>"))
-    # 6N temp
-    print(getSimpleParts(arr[6], "<td class='first_in_group positive'>", "</td>"))
-    # 7N pressure
-    print(getSimpleParts(arr[7], "<td>", "</td>"))
-    # 10N wind
-    print(getSimpleParts(arr[10], "<td><span>", "</span></td>"))
+        # 0 counter
+        print(getSimpleParts(arr[0], "<td class=first>", "</td>"))
+        # 1D temp
+        print(getSimpleParts(arr[1], "<td class='first_in_group positive'>", "</td>"))
+        # 2D pressure
+        print(getSimpleParts(arr[2], "<td>", "</td>"))
+        # 5D wind
+        print(getSimpleParts(arr[5], "<td><span>", "</span></td>"))
+        # 6N temp
+        print(getSimpleParts(arr[6], "<td class='first_in_group positive'>", "</td>"))
+        # 7N pressure
+        print(getSimpleParts(arr[7], "<td>", "</td>"))
+        # 10N wind
+        print(getSimpleParts(arr[10], "<td><span>", "</span></td>"))
+        
+        print("--------------------")
 
 
 # 0         COUNTER
